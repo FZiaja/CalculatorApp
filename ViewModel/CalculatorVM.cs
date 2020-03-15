3b4f19add4a858bc25330f021cad4793940edc34
Filip Ziaja <filip.ziaja@gmail.com>
2020-03-15 16:22:47 -0400

    Test

---
+++ ViewModel/CalculatorVM.cs
@@ -0,0 +1,105 @@
+﻿using CalculatorApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorApp.ViewModel
+{
+	public class CalculatorVM : INotifyPropertyChanged
+    {
+		private string displayString;
+
+		public string DisplayString
+		{
+			get { return displayString; }
+			set 
+			{ 
+				displayString = value;
+				OnPropertyChanged("DisplayString");
+			}
+		}
+
+		private double number1;
+
+		public double Number1
+		{
+			get { return number1; }
+			set { number1 = value; }
+		}
+
+		private double number2;
+
+		public double Number2
+		{
+			get { return number2; }
+			set { number2 = value; }
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public NumberButtonCommand NumberButtonCommand { get; set; }
+
+		public CalculatorVM()
+		{
+			NumberButtonCommand = new NumberButtonCommand(this);
+			DisplayString = "0";
+		}
+
+		public void NumberButtonClick(object parameter)
+		{
+			//MessageBox.Show(parameter.ToString());
+			//MessageBox.Show("---" + DisplayString + "---");
+			string number = parameter as string;
+
+			if (DisplayString == "0")
+			{
+				DisplayString = number;
+			}
+			else
+			{
+				DisplayString = DisplayString + number;
+			}
+			//MessageBox.Show("---" + DisplayString + "---");
+		}
+
+		public void DecimalButtonClick()
+		{
+
+		}
+
+		public void ACButtonClick()
+		{
+
+		}
+
+		public void NegativeButtonClick()
+		{
+
+		}
+
+		public void PercentButtonClick()
+		{
+
+		}
+
+		public void OperatorButtonClick()
+		{
+
+		}
+
+		public void EqualsButtonClick()
+		{
+
+		}
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ ViewModel/Commands/NumberButtonCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CalculatorApp.ViewModel.Commands
+{
+    public class NumberButtonCommand : ICommand
+    {
+        public CalculatorVM VM { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public NumberButtonCommand(CalculatorVM vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VM.NumberButtonClick(parameter);
+        }
+    }
+}
